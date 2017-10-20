c8a10730fec9d7a641bd855a0f4992a3d459992d
Marcio-RF00 <marciuflad@gmail.com>
2017-10-20 18:21:19 -0200

    Modificado usuario e index.php

--- index.php
+++ index.php
@@ -2,10 +2,26 @@
 
 require_once("config.php");
 
-$user = new Usuario();
-
+/*--COMANDOS de busca--*/
+//traz apenas uma linha por vez
+/*$user = new Usuario();
 $user -> loadById(1);
+echo $user;*/
 
-echo $user;
+//traz uma lista
+/*$lista = Usuario::getList();
+echo json_encode($lista);*/
+
+//traz uma lista de usuarios buscado pelo login
+/*$search = Usuario::search("r");
+echo json_encode($search);*/
+
+//traz um usuario buscado pelo login e senha(autenticados)
+$usuario = new Usuario();
+$usuario -> login("fabiano", "1234");
+echo $usuario;
+
+
+
 
 ?>
